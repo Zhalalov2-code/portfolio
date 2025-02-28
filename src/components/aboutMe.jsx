55b667a6cda54ecb1ffcd3f0d50e60f9c2014c93
Zhalalov2 <fill_flls@icloud.com>
2025-02-28 17:30:40 +0600

    update fon-body

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -5,7 +5,6 @@ function AboutMe() {
         <div id='about'>
             <div className="container aboutMe">
                 <div className="div1 row">
-                    {/* Левый блок */}
                     <div className="col-5 col-md-12 col">
                         <div>
                             <h4 className="hello">Hello, I am</h4>
@@ -15,12 +14,11 @@ function AboutMe() {
                         <div className="about">
                             <h5 className="nameAbout"><b>aboutMe()</b></h5>
                             <p>
-    My name is Zalkarbek, a front-end developer with a passion for building responsive and user-friendly web applications. I started in IT in 2018 as a self-taught Python programmer and graduated with a degree in "Automated Systems and Control" in 2021, followed by a degree in "Internet Technologies and Management" in 2024. I have experience with HTML, CSS, JavaScript, React, and FastAPI, and completed internships at Deutsche Post in 2023 and 2024, where I further developed my front-end skills. I'm also expanding my knowledge in back-end development and aiming to become a full-stack developer.
-</p>
+                                My name is Zalkarbek, a front-end developer with a passion for building responsive and user-friendly web applications. I started in IT in 2018 as a self-taught Python programmer and graduated with a degree in "Automated Systems and Control" in 2021, followed by a degree in "Internet Technologies and Management" in 2024. I have experience with HTML, CSS, JavaScript, React, and FastAPI, and completed internships at Deutsche Post in 2023 and 2024, where I further developed my front-end skills. I'm also expanding my knowledge in back-end development and aiming to become a full-stack developer.
+                            </p>
 
                         </div>
                     </div>
-                    {/* Правый блок */}
                     <div className="col-5 col-md-12 offset-md-2">
                         <div className="zalkarFoto"></div>
                         <div className="learning">
